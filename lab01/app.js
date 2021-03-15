8646aba73235e5e3ba3d7f2c19d13a694cbb7d86
ralph bunker <rbunker@miu.edu>
2021-03-15 16:34:48 -0500

    First commit

---
+++ lab01/app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+alert("Lab01 javascript");
+
+function pow(x, n) {
+	return x ** n;
+}
+
+function add2(n1, n2) {
+	return n1 + n2;
+}
+
+function add(...args) {
+	let count = arguments.length;
+	let sum = 0;
+	for (let i=0; i<count; i++) {
+		sum += arguments[i];		
+	}
+	
+	return sum;
+}
