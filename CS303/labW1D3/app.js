40214f016a1a563639429cde31e0d42b581b1a59
ralphbunker <rbunker@crucimetrics.com>
2021-03-24 14:12:45 -0500

    My lab 3 solution

--- CS303/labW1D3/app.js
+++ CS303/labW1D3/app.js
@@ -1,4 +1,29 @@
-function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D3";
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+
+/*
+Function analyzer
+Has an unbound this
+Creates an object that contains the following information from the "this" object.
+{
+	numProperties  	// number of properties
+	cntShortName	// count of property names shorter than 3 characters
+	cntReferences	// count of reference properties (use the typeof operator to compute this)
+}	
+*/
+function analyzer() {
+	return {};
+}
+
+/* Constructor for a person object
+	Person(name, country, grades) creates object
+	{
+		name: name,  // person's name
+		county: country, // person's country
+		grades: grades,  // integer array of grades
+		computeGrade     // function that computes the grade from grades
+	}
+*/
+function Person(name, country, grades) {
 }
--- CS303/labW1D3/test.js
+++ CS303/labW1D3/test.js
@@ -1,1 +1,37 @@
-/* mocha tests */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable require-jsdoc */
+"use strict";
+
+function mochaTests() {
+    describe("Person object", function () {
+        it("has name", function () {
+            assert.equal(new Person("bob", "usa", [100, 100]).name, "bob");
+        });
+	});
+
+    describe("Person object", function () {
+        it("has country", function () {
+            assert.equal(new Person("bob", "usa", [100, 100]).country, "usa");
+        });
+	});
+	
+    describe("Person object", function () {
+        it("has grade", function () {
+            assert.equal(new Person("bob", "usa", [100, 90]).computeGrade(), 95);
+        });
+	});	
+
+    describe("Analyser", function () {
+        it("analysis properties", function () {
+			let person = new Person("bob", "usa", [100, 90]);
+			person.f = analyzer;	// name too short
+			person.x = 0;			// name too short
+			let analysis = person.f();
+            assert.equal(analysis.numProperties, 6);
+			assert.equal(analysis.cntShortName, 2);
+			assert.equal(analysis.cntReference, 1);	// the grades array is a reference.
+        });
+	});	
+	
+}
